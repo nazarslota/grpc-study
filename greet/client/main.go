7ff6a5d8697d538c51f7e69f160881a850521ba6
udholdenhed <nazarslota03@gmail.com>
2022-12-23 18:04:43 +0200

    Server and client. Generated proto files.

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const addr = "5051"
+
 func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer func(conn *grpc.ClientConn) { _ = conn.Close() }(conn)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"net"
+
+	pb "github.com/udholdenhed/grpc-study/greet/proto"
+	"google.golang.org/grpc"
+)
+
+const addr = "0.0.0.0:5051"
+
+type server struct {
+	pb.GreetServiceServer
+}
+
 func main() {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	if err := s.Serve(listener); err != nil {
+		panic(err)
+	}
 }
