7e54a3ea50d8597ea52bd326c804a2f883339ad0
udholdenhed <nazarslota03@gmail.com>
2022-12-23 19:38:46 +0200

    Greet client implementation.

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/udholdenhed/grpc-study/greet/proto"
 )
 
-const addr = "5051"
+const addr = "0.0.0.0:5051"
 
 func main() {
 	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -14,4 +19,16 @@ func main() {
 	}
 	defer func(conn *grpc.ClientConn) { _ = conn.Close() }(conn)
 
+	client := pb.NewGreetServiceClient(conn)
+	DoGreat(client)
+}
+
+func DoGreat(client pb.GreetServiceClient) {
+	response, err := client.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Nazar",
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(response.Result)
 }
